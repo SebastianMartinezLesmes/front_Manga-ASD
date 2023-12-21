f8adf36ee43db6f8ea3be0b1b607025de841412a
SebastianMartinezLesmes <120604491+SebastianMartinezLesmes@users.noreply.github.com>
2023-12-21 11:31:24 -0500

    Add files via upload

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -34,7 +34,7 @@ export class HomePage {
   password: string = '';  
 
   ventana: string = '';
-  number: any = [{},{},{},{}];
+  number: any = [{id: 1},{id: 2},{id: 3},{id: 4}];
 
   title: string = '';
   description: string = '';
@@ -59,21 +59,13 @@ export class HomePage {
   mostrarregistro(){this.ventana = 'registro'; this.changeList();}
   mostrarCrearUsuario(){this.ventana = 'crear_usuario'; this.changeList();}
   mostrarCrearManga(){this.ventana = 'crear_manga'; this.changeList();}
+  mostrarMisMangas(){this.ventana = 'mis_manga'; this.changeList();}
+
   questInto(){
     if(this.cargo === ''|| this.cargo === ' '){
       this.cargo ='visitante'
     } 
   }
-
-  usuariosDB: any = [];
-  getUser() {
-    this.http.get('').subscribe(
-      (response) => {
-        console.log('Respuesta del servidor:', response);
-        this.usuariosDB = response;
-      },(error) => {console.error('Error al obtener datos del servidor:', error);}
-    )
-  };
 
   crearManga(){
     console.log(this.title+' '+this.description+' '+this.cant+' '+this.price)
@@ -81,15 +73,49 @@ export class HomePage {
     this.limpiar();
   };
 
-   async crearUsuario(){
-    console.log('Datos del usuario:', {
-      name: this.name = this.name,
-      lastName: this.lastName = this.lastName,
-      email: this.email = this.email,
-      celular: this.celular = this.celular,
-      otherName: this.otherName = this.otherName,
-      password: this.password = this.password,
-    })
+  async crearUsuario(){
+    
+    let camposVacios = '';
+    let cont = 0;
+  
+    if (this.name === '') {
+      camposVacios += 'Nombre, ';
+      cont = cont + 1;
+    }
+    if (this.lastName === '') {
+      camposVacios += 'Apellido, ';
+      cont = cont + 1;
+    }
+    if (this.email === '') {
+      camposVacios += 'Email, ';
+      cont = cont + 1;
+    }
+    if (this.celular === '') {
+      camposVacios += 'Celular, ';
+      cont = cont + 1;
+    }
+    if (this.otherName === '') {
+      camposVacios += 'username, ';
+      cont = cont + 1;
+    }
+    if (this.password === '') {
+      camposVacios += 'Contraseña, ';
+      cont = cont + 1;
+    }
+  
+    camposVacios = camposVacios.slice(0, -2); // Eliminar la coma y el espacio al final
+    if (cont !== 0){ this.presentAlert("Los siguientes campos son requeridos para ingresar a la paguina",""+ camposVacios ); }
+    else { 
+      console.log('Datos del usuario:', {
+        name: this.name = this.name,
+        lastName: this.lastName = this.lastName,
+        email: this.email = this.email,
+        celular: this.celular = this.celular,
+        otherName: this.otherName = this.otherName,
+        password: this.password = this.password,
+      })
+      this.presentAlert("El campo "+ camposVacios, "Es requerido para ingresar a la paguina" 
+    );}
   };
 
   changeList() {
@@ -98,21 +124,20 @@ export class HomePage {
     } else if (this.list === 'false') {
       this.list = 'true';
     }
-  }
-
+  };
   async presentAlert(title: string, message: string) {
     const alert = await this.alertController.create({
       header: title,
       message: message,
       buttons: ['OK'],
       mode: 'ios'
-    })
+    });
+    await alert.present();
   }
   
   login() {
     const inputUsername = this.email;
     const inputPassword = this.password;
-    console.log(inputUsername+' '+inputPassword)
     const user = this.usuariosDB.find((u: any) => u.numeroTI === inputUsername && u.credenciales === inputPassword);
 
     if (this.email === '' || this.password === '') {
@@ -134,9 +159,13 @@ export class HomePage {
       else { this.presentAlert("El campo "+ camposVacios, "Es requerido para ingresar a la paguina" );}
     }
     else{
+      this.cargo ='cliente';
+      this.ventana = '';
+      this.presentAlert("usuario registrado", "ahora puedes alquilar tu manga");
       if (user) {
         console.log(user.email+' '+user.password)
-        this.cargo = user.role
+        //this.cargo = user.role
+        this.cargo ='cliente';
       } else { this.presentAlert("usuario inexistente", ""); }
     } this.limpiar();
   };
@@ -155,4 +184,44 @@ export class HomePage {
       reader.readAsDataURL(file);
     }
   };
+
+  async alquilar(){ const alert = await this.alertController.create({
+    header: '¿Está seguro de que desea alquilar este manga?',
+    message: 'Despues de alquilarlo tendras 15 dias para devolverlo.',
+    buttons: [
+      {
+        text: 'Cancelar',
+        cssClass: 'secondary',
+        handler: () => {}
+      },
+      {
+        text: 'Aceptar',
+        handler: () => { this.presentAlert ("Manga alquilado", "Disfruta de tu manga y no se te olvide devolverlo."); }
+      }], mode: 'ios'
+    });    await alert.present();
+  };
+
+  usuariosDB: any = [];
+  getUser() {
+    this.http.get('').subscribe(
+      (response) => {
+        console.log('Respuesta del servidor:', response);
+        this.usuariosDB = response;
+      },(error) => {console.error('Error al obtener datos del servidor:', error);}
+    )
+  };
+
+  devolver(dev:any){
+
+    // Define los datos que deseas enviar al servidor
+    const data = {id: dev };
+    console.log(data.id);
+
+    // Realiza la solicitud POST al servidor PHP
+    this.http.post('', data).subscribe(response => {
+      console.log('Respuesta del servidor:', response);
+    }, error => {
+      console.error('Error al enviar el ID al servidor:', error);
+    });
+  }
 }
